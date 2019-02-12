8fdfedbc8db05d6d052c76358e35ba4d8793bc4f
Valentino Decaroli <caronte.ade@gmail.com>
2019-02-12 22:10:17 +0100

    Fixed
    Correct and verified translation, Fixed UTF8 decoding, Time Zone.

--- pH7-Internationalization-master/_protected/app/langs/it_IT/language.php
+++ pH7-Internationalization-master/_protected/app/langs/it_IT/language.php
@@ -1,14 +1,15 @@
-﻿<?php
+<?php
 /**
  * @author         Pierre-Henry Soria <ph7software@gmail.com>
- * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2019, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package        PH7 / App / Lang / it IT
+ * @package        PH7 / App / Lang / It IT
  */
 
 namespace PH7;
 
 defined('PH7') or exit('Restricted access');
+
 
 Framework\Registry\Registry::getInstance()->lang = [
 
